36d6449546e795303a7b0acf6fedb31dc859057c
Anwesh43 <anweshthecool0@gmail.com>
2020-12-08 14:26:49 +0530

    adding TRectToBeaker class

--- index.ts
+++ index.ts
@@ -185,3 +185,26 @@ class TRBNode {
         return this 
     }
 }
+
+class TRectToBeaker {
+    
+    curr : TRBNode = new TRBNode(0)
+    dir : number = 1 
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)    
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
